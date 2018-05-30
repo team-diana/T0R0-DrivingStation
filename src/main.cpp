5a1526cb6de139c37e7cbf5687a35dd006b07323
Unknown <adriansagerlaganga@gmail.com>
2018-05-30 20:27:08 +0200

    Auto stash before rebase of "sager-gstreamer"

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,21 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setApplicationName("T0R0 GUI DRIVING STATION");
     QApplication app(argc, argv);
     QGst::init(&argc, &argv);
 
     Window window;
-    window.show();
+    window.showFullScreen();
+
+
+    //show message on console.
+    qDebug() << "\nWindow opened successfully\n";
+
+
     window.startVideo();
+
+    qDebug() << "\nVideo started successfully\n";
 
     return app.exec();
 }
--- src/window.h
+++ src/window.h
@@ -22,8 +22,8 @@ class Window : public QWidget
 	Q_OBJECT
 
 public:
-    explicit Window(QWidget *parent = 0);
-		~Window();
+  explicit Window(QWidget *parent = 0);
+	~Window();
 
 public Q_SLOTS:
 	//Gstreamer://
@@ -34,14 +34,14 @@ protected:
 	void keyPressEvent(QKeyEvent *e);
 
 private:
-    bool connected;
-		//Gstreamer://
-    QGst::PipelinePtr m_pipeline;
-		QGst::Ui::VideoWidget *m_videoWidget;
+  bool connected;
+	//Gstreamer://
+  QGst::PipelinePtr m_pipeline;
+	QGst::Ui::VideoWidget *m_videoWidget;
 
-		void onRtpBinPadAdded(const QGst::PadPtr & pad);
-    void onBusErrorMessage(const QGst::MessagePtr & msg);
-		//////////////
+	void onRtpBinPadAdded(const QGst::PadPtr & pad);
+  void onBusErrorMessage(const QGst::MessagePtr & msg);
+	//////////////
 };
 
 #endif // WINDOW_H
