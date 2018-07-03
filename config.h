7dc2a6e0a508631013191ee1913c0e4ee9f3dfec
DunkelAmber <frama95@gmail.com>
2018-07-03 10:10:41 +0200

    Added configuration header

---
+++ config.h
@@ -0,0 +1,13 @@
+/****************************************************************************
+**
+** T0-R0 Driving Station Configuration
+**
+** This file contains the main configuration of T0-R0 Rover Driving Station.
+** Modify here to set up all main variables of this code.
+**
+****************************************************************************/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#endif // CONFIG_H
