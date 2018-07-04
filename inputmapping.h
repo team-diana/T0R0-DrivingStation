bf4715b79db9190ee9a31a252d4cee6a7e00ebcf
DunkelAmber <frama95@gmail.com>
2018-07-04 17:17:27 +0200

    Added Input Map

---
+++ inputmapping.h
@@ -0,0 +1,67 @@
+#ifndef INPUTMAPPING_H
+#define INPUTMAPPING_H
+
+//* JOYSTICK MAP *//
+// Axis
+#define JOYSTICK_PITCH 0
+#define JOYSTICK_ROLL 1
+#define JOYSTICK_YAW 2
+#define JOYSTICK_THROTTLE 3
+
+#define JOYSTICK_ARROWLR 4  // L = -32767   R = 32767
+#define JOYSTICK_ARROWUD 5  // D = -32767   U = 32767
+
+// Buttons
+#define JOYSTICK_PITCH 0
+#define JOYSTICK_ROLL 1
+#define JOYSTICK_YAW 2
+#define JOYSTICK_THROTTLE 3
+
+#define JOYSTICK_0 0    // Fire button
+
+#define JOYSTICK_1 1    // Thumb button
+
+#define JOYSTICK_3 2    // Butons on top
+#define JOYSTICK_4 3
+#define JOYSTICK_5 4
+#define JOYSTICK_6 5
+
+#define JOYSTICK_7 6    // Butons on base
+#define JOYSTICK_8 7
+#define JOYSTICK_9 8
+#define JOYSTICK_10 9
+#define JOYSTICK_11 10
+#define JOYSTICK_12 11
+//////////
+
+//* GAMEPAD MAP *//
+// Axis
+#define GAMEPAD_L3X 0
+#define GAMEPAD_L3Y 1
+#define GAMEPAD_R3X 3
+#define GAMEPAD_R3Y 4
+
+#define GAMEPAD_L1 2
+#define GAMEPAD_R1 5
+
+#define GAMEPAD_ARROWLR 6   // L = -32767   R = 32767
+#define GAMEPAD_ARROWUD 7   // D = -32767   U = 32767
+
+// Buttons
+#define GAMEPAD_L2 4
+#define GAMEPAD_R2 5
+
+#define GAMEPAD_A 0
+#define GAMEPAD_B 2
+#define GAMEPAD_X 1
+#define GAMEPAD_Y 3
+
+#define GAMEPAD_BACK 6
+#define GAMEPAD_START 7
+#define GAMEPAD_SPECIAL 8
+
+#define GAMEPAD_L3 9
+#define GAMEPAD_R3 10
+//////////
+
+#endif // INPUTMAPPING_H
