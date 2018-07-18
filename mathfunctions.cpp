aeb4c1ad4f7316e2114175291c913b62df43eb6f
DunkelAmber <frama95@gmail.com>
2018-07-18 15:35:00 +0200

    feat(math): improve math behind input HID

--- mathfunctions.cpp
+++ mathfunctions.cpp
@@ -1,15 +1,17 @@
 #include "mathfunctions.h"
 #include <math.h>
 
-/*math::math()
-{
 
-}*/
-
+/*
+ * The function that is implemented
+ * Compy this on Geogebra after creating three sliders (a, b, c)
+ * Se(x < (-(1 + a)) / b, -1, (-(1 + a)) / b < x < (-a) / b, -abs((b x + a)^c), (-a) / b < x < a / b, 0, a / b < x < (1 + a) / b, (b x - a)^c, x > (1 + a) / b, 1)
+ */
 double inputLinearityFunction (double x, float a, float b, int c)
 {
     float minFactor = a / b;
     float maxFactor = (1+a) / b;
+
     double ans = 0;
 
     if (x < -maxFactor)
@@ -18,7 +20,8 @@ double inputLinearityFunction (double x, float a, float b, int c)
     }
     else if ( x > -maxFactor && x < -minFactor)
     {
-        ans =  (double) pow (( -(1+a) / b ), c);
+        // -abs((b x + a)^c)
+        ans =  (double) pow ( (double)(-abs(b*x + a)) , c);
     }
     else if ( x > -minFactor && x < minFactor)
     {
@@ -26,7 +29,7 @@ double inputLinearityFunction (double x, float a, float b, int c)
     }
     else if ( x > minFactor && x < maxFactor)
     {
-        ans = (double) pow(( (1+a) / b ), c);
+        ans = (double) pow( (double)(b x - a), c);
     }
     else
     {
